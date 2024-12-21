3608b8f072b16e9ff48742fbb093578272e29dab
Evan Cole <ecole96@outlook.com>
2024-12-21 13:09:06 -0500

    tweaks

--- init.sh
+++ init.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-# take down containers (if running)
-docker compose -f ./media-homeserver.yml down
-
 # reset dirs
-rm -r -f ./config
-rm -r -f ./downloads
-rm -r -f ./movies
-rm -r -f ./tv
+rm -rf ./config
+rm -rf ./downloads
+rm -rf ./movies
+rm -rf ./tv
 
 # initialize new config dirs
 mkdir -p ./config/gluetun
@@ -26,6 +23,3 @@ mkdir -p ./config/sonarr
 mkdir -p ./downloads
 mkdir -p ./movies
 mkdir -p ./tv
-
-# start containers
-docker compose -f ./media-homeserver.yml up -d
