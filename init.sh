d0f155f2722bea9442b89e9e7bd5dd7e323cba83
Evan Cole <ecole96@outlook.com>
2025-09-14 22:47:45 -0400

    cloudflare tunnel setup

--- init.sh
+++ init.sh
@@ -11,13 +11,15 @@ rm -rf ./tv
 # initialize new config dirs
 mkdir -p ./config/gluetun
 mkdir -p ./config/jellyfin
-mkdir -p ./config/letsencrypt
-mkdir -p ./config/npm
 mkdir -p ./config/portainer
 mkdir -p ./config/prowlarr
 mkdir -p ./config/qbittorrent
 mkdir -p ./config/radarr
 mkdir -p ./config/sonarr
+mkdir -p ./config/cloudflared
+
+# initialize cloudflared config (you will need to edit this before launching the Docker stack)
+cp ./cloudflared_config.yml ./config/cloudflared/config.yml
 
 # initialize content folders
 mkdir -p ./downloads
--- test-init.sh
+++ test-init.sh
@@ -8,16 +8,18 @@ rm -rf ./data
 
 # initialize new config dirs
 mkdir -p ./config/jellyfin
-mkdir -p ./config/letsencrypt
-mkdir -p ./config/npm
 mkdir -p ./config/portainer
 mkdir -p ./config/prowlarr
 mkdir -p ./config/qbittorrent
 mkdir -p ./config/radarr
 mkdir -p ./config/sonarr
+mkdir -p ./config/cloudflared
+
+# initialize cloudflared config (you will need to edit this before launching the Docker stack)
+cp ./cloudflared_config.yml ./config/cloudflared/config.yml
 
 # initialize content folders
 mkdir -p ./data/media/movies
 mkdir -p ./data/media/tv
-mkdir -p ./torrents/movies
-mkdir -p ./torrents/tv
+mkdir -p ./data/torrents/movies
+mkdir -p ./data/torrents/tv
